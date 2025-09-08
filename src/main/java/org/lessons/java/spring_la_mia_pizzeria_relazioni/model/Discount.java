b3ff6732a072730d5d78f04bd3f37468035832e4
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-08 13:28:15 +0200

    feat: create Discount entity with ManyToOne relations to Pizza id

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Discount.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Discount.java
@@ -1,5 +1,81 @@
 package org.lessons.java.spring_la_mia_pizzeria_relazioni.model;
 
-public class Discount {
+import java.math.BigDecimal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "discounts")
+public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The start time cannot be blank or empty")
+    private String startTime;
+
+    @NotBlank(message = "The end time cannot be blank or empty")
+    private String endTime;
+
+    @Lob
+    private String title;
+
+    @NotNull
+    @Min(value = 0, message = "The Value cannot be negative")
+    private BigDecimal value;
+
+    // collegamento tabella pizza
+    @ManyToOne
+    @JoinColumn(name = "pizza_id", nullable = false)
+    private Pizza pizza;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return this.endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BigDecimal getValue() {
+        return this.value;
+    }
+
+    public void setEndTime(BigDecimal value) {
+        this.value = value;
+    }
 }
