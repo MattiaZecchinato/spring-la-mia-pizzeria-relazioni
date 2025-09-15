d7046a1ac7140f06acfe1c2e4f6b6e703400d124
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-09-15 13:59:58 +0200

    feat: update many-to-many relationship between Ingredient and Pizza entities

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Ingredient.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Ingredient.java
@@ -6,8 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -27,8 +25,7 @@ public class Ingredient {
     @Lob
     private String description;
 
-    @ManyToMany
-    @JoinTable(name = "ingredient_pizza", joinColumns = @JoinColumn(name = "pizza_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
+    @ManyToMany(mappedBy = "ingredients")
     private List<Pizza> pizzas;
 
     public int getId() {
--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Pizza.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_relazioni/model/Pizza.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
@@ -38,7 +40,8 @@ public class Pizza {
     @OneToMany(mappedBy = "pizza")
     private List<Discount> discounts;
 
-    @ManyToMany(mappedBy = "pizzas")
+    @ManyToMany
+    @JoinTable(name = "ingredient_pizza", joinColumns = @JoinColumn(name = "pizza_id"), inverseJoinColumns = @JoinColumn(name = "ingredient_id"))
     private List<Ingredient> ingredients;
 
     public int getId() {
